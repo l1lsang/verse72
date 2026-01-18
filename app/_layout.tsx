a04d756e30a0eb27b78537db1ad3628542ad3c96
codingquokka <104996819+l1lsang@users.noreply.github.com>
2026-01-19 07:33:53 +0900

    메시지

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
-
 import { ThemeProvider } from "@/src/theme/ThemeProvider";
+import { initializeKakaoSDK } from "@react-native-kakao/core";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-
+  useEffect(() => {
+    initializeKakaoSDK({
+      appKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY!,
+    });
+  }, []);
 
   return (
     <ThemeProvider>
