40b9ee5041d556f3616e875bbe4f1c13a149c40a
codingquokka <104996819+l1lsang@users.noreply.github.com>
2026-01-19 08:02:15 +0900

    메시지

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "@/src/theme/ThemeProvider";
-import { initializeKakaoSDK } from "@react-native-kakao/core";
+import { getKeyHash, initializeKakaoSDK } from "@react-native-kakao/core";
 import { Stack } from "expo-router";
 import { useEffect } from "react";
+import { Alert } from "react-native";
 
 export default function RootLayout() {
   useEffect(() => {
     initializeKakaoSDK({
       appKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY!,
+    });
+
+    // 🔥 플랜 B: 키 해시 직접 출력
+    getKeyHash().then((hash) => {
+      console.log("🔥 Kakao KeyHash:", hash);
+      Alert.alert("Kakao KeyHash", hash);
     });
   }, []);
 
@@ -16,3 +23,4 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
+
